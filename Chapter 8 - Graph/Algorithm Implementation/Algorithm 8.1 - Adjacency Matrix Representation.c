e8d82b2bb782ae3f2889c3929b2a6a856c55b431
Mahir Faysal Tusher <www.mahirfaysaltushar@gmail.com>
2026-03-14 22:59:55 +0600

    Create Algorithm 8.1 - Adjacency Matrix Representation.c

---
+++ Chapter 8 - Graph/Algorithm Implementation/Algorithm 8.1 - Adjacency Matrix Representation.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+#define MAX 10   // maximum number of vertices
+
+int adj[MAX][MAX];  // the adjacency matrix
+int n;              // number of vertices
+
+// Add an edge between vertex u and vertex v (undirected)
+void addEdge(int u, int v) {
+    adj[u][v] = 1;
+    adj[v][u] = 1;  // remove this line for directed graph
+}
+
+// Print the adjacency matrix
+void printMatrix() {
+    printf("Adjacency Matrix:\n");
+    printf("   ");
+    for (int i = 0; i < n; i++) printf("%d ", i);
+    printf("\n");
+
+    for (int i = 0; i < n; i++) {
+        printf("%d: ", i);
+        for (int j = 0; j < n; j++) {
+            printf("%d ", adj[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    n = 5;  // 5 vertices: 0, 1, 2, 3, 4
+
+    addEdge(0, 1);
+    addEdge(0, 4);
+    addEdge(1, 2);
+    addEdge(1, 3);
+    addEdge(1, 4);
+    addEdge(2, 3);
+    addEdge(3, 4);
+
+    printMatrix();
+    return 0;
+}
