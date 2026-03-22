775708e6fa79e99d18d6ece5aece6ae3ab0056a4
Mahir Faysal Tusher <www.mahirfaysaltushar@gmail.com>
2026-03-22 22:54:22 +0600

    Update 1 - Algorithm 4.1.c

--- Chapter 4 - Arrays, Records and Pointers/Algorithm Implementation/1 - Algorithm 4.1.c
+++ Chapter 4 - Arrays, Records and Pointers/Algorithm Implementation/1 - Algorithm 4.1.c
@@ -1,27 +1,18 @@
-#include<stdio.h>
+#include <stdio.h?
+
 int main()
 {
-    int LA[7] = {0, 247, 56, 429, 135, 87, 156};
-    int LB = 1;
-    int UB = 6;
+    int arr[5] = {10, 20, 30, 40, 50};
+    int LB = 0;
+    int UB = 4;
+    
     int K = LB;
-    int sum = 0;
-    int count = 0;
-
-    printf("Traversing array elements:\n");
-
+    
     while(K <= UB)
     {
-        printf("LA[%d] = %d\n", K, LA[K]);
-        sum += LA[K];
-        if(LA[K] > 100)
-        {
-            count ++;
-        }
-        K++;
+        printf("%d\n", arr[K]);
+        K = K + 1;
     }
-    printf("\nSum of array elements: %d\n", sum);
-    printf("Count of elements greater than 100: %d\n", count);
-    printf("Average of array elements: %2f\n", (float)sum / (UB - LB + 1));
+    
     return 0;
 }
