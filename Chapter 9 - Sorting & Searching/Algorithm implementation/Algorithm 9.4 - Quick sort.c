64097a8627652a3577186a492afda605986ece5d
Mahir Faysal Tusher <www.mahirfaysaltushar@gmail.com>
2026-04-01 22:58:04 +0600

    Update Algorithm 9.4 - Quick sort.c

--- Chapter 9 - Sorting & Searching/Algorithm implementation/Algorithm 9.4 - Quick sort.c
+++ Chapter 9 - Sorting & Searching/Algorithm implementation/Algorithm 9.4 - Quick sort.c
@@ -1,61 +1,71 @@
-#include <stdio.h>
+#include<stdio.h>
 
-int makepart(int A[], int first, int last)
+void swap(int *a, int *b)
 {
-    int part_value = A[first];
-    int i = first;
-    int j = last;
-    int temp;
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
-    do
+int partition(int arr[], int low, int high)
+{
+    int pivot = arr[low];
+    int p = low + 1;
+    int q = high;
+
+    while(p <= q)
     {
-        do
+        while(p <= high && arr[p] <= pivot)
         {
-            i = i + 1;
-        } while (i < last && A[i] <= part_value);
-
-        do
-        {
-            j = j - 1;
-        } while (A[j] > part_value);
-
-        if (i < j)
-        {
-            temp = A[i];
-            A[i] = A[j];
-            A[j] = temp;
+            p++;
         }
-
-    } while (i < j);
-
-    A[first] = A[j];
-    A[j] = part_value;
-
-    return j;
+        while(q >= low && arr[q] > pivot)
+        {
+            q--;
+        }
+        if(p < q)
+        {
+            swap(&arr[p], &arr[q]);
+        }
+    }
+    swap(&arr[low], &arr[q]);
+    return q;
 }
 
-void Quick_sort(int A[], int f, int l)
+void quicksort(int arr[], int low, int high)
 {
-    if (f < l)
+    if(low < high)
     {
-        int j = makepart(A, f, l + 1);
-        Quick_sort(A, f, j - 1);
-        Quick_sort(A, j + 1, l);
+        int pivotIndex = partition(arr, low, high);
+
+        quicksort(arr, low, pivotIndex - 1);
+
+        quicksort(arr, pivotIndex + 1, high);
     }
 }
 
-int main()
+void printArray(int arr[], int size) 
 {
-    int A[] = {25, 57, 48, 37, 12, 92, 86, 33};
-    int n = 8;
-
-    Quick_sort(A, 0, n - 1);
-
-    printf("Sorted Array:\n");
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < size; i++) 
     {
-        printf("%d ", A[i]);
+        printf("%d ", arr[i]);
     }
+    printf("\n");
+}
+
+int main() 
+{
+
+    int arr[] = {35, 50, 15, 25, 80, 20, 90, 45};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    printf("Unsorted array: \n");
+    printArray(arr, n);
+
+    quickSort(arr, 0, n - 1);
+
+    printf("Sorted array: \n");
+    printArray(arr, n);
 
     return 0;
 }
