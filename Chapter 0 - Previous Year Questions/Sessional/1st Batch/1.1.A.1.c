66b578d1a4fed04f321f5c4f7f5a571f8463511f
Mahir Faysal Tusher <www.mahirfaysaltushar@gmail.com>
2026-04-02 09:58:45 +0600

    Update 1.1.A.1.c

--- Chapter 0 - Previous Year Questions/Sessional/1st Batch/1.1.A.1.c
+++ Chapter 0 - Previous Year Questions/Sessional/1st Batch/1.1.A.1.c
@@ -3,53 +3,58 @@
 Write a program to translate the binary search and insertion algorithm into a subprogram BINARY(ARRAY, LB, UB, ITEM, LOC) 
 which finds either the location LOC where ITEM appears in ARRAY or the location LOC where ITEM should be inserted into ARRAY. (60)
 */
-
 #include <stdio.h>
 
-int BINARY(int ARRAY[], int LB, int UB, int ITEM) 
+void BINARY(int ARRAY[], int LB, int UB, int ITEM, int *LOC)
 {
     int BEG = LB;
     int END = UB;
     int MID;
 
-    while (BEG <= END) 
+    while (BEG <= END)
     {
         MID = (BEG + END) / 2;
 
-        if (ARRAY[MID] == ITEM) 
+        if (ARRAY[MID] == ITEM)
         {
-            printf("Item %d found at location: %d\n", ITEM, MID);
-            return MID;          
-        } 
-        else if (ITEM < ARRAY[MID]) 
+            *LOC = MID;   // Found location
+            return;
+        }
+        else if (ITEM < ARRAY[MID])
         {
             END = MID - 1;
-        } 
-        else 
+        }
+        else
         {
             BEG = MID + 1;
         }
     }
 
-    printf("Item %d not found. It should be inserted at location: %d\n", ITEM, BEG);
-    return BEG;
+    // Not found → insertion position
+    *LOC = BEG;
 }
 
-int main() {
+int main()
+{
     int ARRAY[] = {10, 20, 30, 40, 50, 60};
     int n = 6;
-    int ITEM;
+    int ITEM, LOC;
 
     printf("Sorted Array: ");
     for (int i = 0; i < n; i++)
-    {
         printf("%d ", ARRAY[i]);
-    } 
+
     printf("\n\nEnter item to search: ");
     scanf("%d", &ITEM);
 
-    int LOC = BINARY(ARRAY, 0, n - 1, ITEM);
-    printf("LOC = %d\n", LOC);
+    // Call BINARY
+    BINARY(ARRAY, 0, n - 1, ITEM, &LOC);
+
+    // Output result
+    if (ARRAY[LOC] == ITEM)
+        printf("Item %d found at location: %d\n", ITEM, LOC);
+    else
+        printf("Item %d not found. It should be inserted at location: %d\n", ITEM, LOC);
 
     return 0;
 }
